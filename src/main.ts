9ca6fa1872579276da64836df8a8d81fdeb353ed
bobrosoft <bobrosoft@yandex.ru>
2025-08-09 21:40:48 +0200

    TASK-20: [Dev] Add reload button

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,21 @@ if (Helpers.isTouchDevice()) {
   addJoystick(k, {size: Math.min(window.innerWidth / 15, 60)});
 }
 
+// Add reload button to the top right corner
+k.loadSprite('reload-button', '/sprites/icons/reload.png');
+const reloadButton = k.add([
+  'reload-button',
+  k.sprite('reload-button'),
+  k.area(),
+  k.pos(k.width() - 10, 10),
+  k.anchor('topright'),
+  k.fixed(),
+  k.stay(),
+]);
+reloadButton.onClick(() => {
+  window.location.reload();
+});
+
 // Watch for orientation changes to adjust canvas size
 window.addEventListener('resize', () => {
   setTimeout(() => {
