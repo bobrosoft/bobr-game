b35a577e94c6ba53ec7b656c62d62e0281602dd3
bobrosoft <bobrosoft@yandex.ru>
2025-09-05 20:59:53 +0200

    TASK-28: [Dev] Add instructions on how to install PWA

--- src/scenes/rotateDevice.ts
+++ src/scenes/rotateDevice.ts
@@ -1,4 +1,4 @@
-import {t} from 'i18next';
+import i18next, {t} from 'i18next';
 import {KCtx} from '../kaplay';
 
 export const sceneRotateDevice = (k: KCtx) => {
@@ -11,7 +11,6 @@ export const sceneRotateDevice = (k: KCtx) => {
     k.pos(k.width() / 2, k.height() / 2 - 10),
     k.scale(k.vec2(-1, 1)),
     k.fixed(),
-    k.stay(),
   ]);
 
   k.add([
@@ -28,6 +27,42 @@ export const sceneRotateDevice = (k: KCtx) => {
     k.anchor('top'),
     k.pos(k.width() / 2, k.height() / 2 + 20),
     k.fixed(),
-    k.stay(),
   ]);
+
+  // Add instructions to install PWA
+  const instructionButton = k.add([
+    //
+    'rotate-device-instructions',
+    k.rect(k.width() * 0.9, 56, {radius: 8, fill: false}),
+    k.outline(2, new k.Color(255, 255, 255)),
+    k.anchor('center'),
+    k.area(),
+    k.pos(k.width() / 2, k.height() * 0.85),
+    k.fixed(),
+  ]);
+
+  instructionButton.add([
+    //
+    k.text(t('common.howToInstall'), {
+      font: 'pixel',
+      size: 14,
+      width: k.width() * 0.8,
+      align: 'center',
+      lineSpacing: 10,
+    }),
+    k.color('white'),
+    k.anchor('center'),
+  ]);
+
+  instructionButton.onClick(() => {
+    setTimeout(() => {
+      let url = 'https://www.cdc.gov/niosh/mining/tools/installpwa.html#cdc_generic_section_2-installing-a-pwa-on-ios';
+      switch (i18next.language) {
+        case 'ru':
+          url = 'https://darkshaurma.com/about/pwa/';
+          break;
+      }
+      window.open(url, '_blank');
+    }, 100); // need timeout or browser may block opening the new tab
+  });
 };
